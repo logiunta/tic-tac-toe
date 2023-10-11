20267489e560e988e9bbd38ddf3c7d0b182a6b4b
Loris <logiunta1997gmail.com>
2023-10-11 16:45:06 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+frame = Tk()
+frame.title("Tic Tac Toe")
+frame.geometry("550x450")
+# Label(frame, text="This is a 550 by 450 Window!", font=("Arial",20,"bold")).pack()
+
+
+
+
+
+
+
+
+
+
+
+frame.mainloop()
